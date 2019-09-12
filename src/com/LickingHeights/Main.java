b3715be60f2febca98777e8894f6a94384a7ba95
Hamza <hamooz7670@gmail.com>
2019-09-12 09:32:14 -0400

    Initial commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,29 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    System.out.println("Hello");
+    System.out.println("Word");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
